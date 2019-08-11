87e6b6f675c928efc900de4f1156522a723681a9
Clemens Lange <clemens.lange@cern.ch>
2019-08-11 18:18:40 +0200

    add TRIGGER_TOKEN

--- services/server/tdr_paperdiff/settings.py
+++ services/server/tdr_paperdiff/settings.py
@@ -6,6 +6,7 @@ config = Config(".env")
 # Define frontend origin for CORS via environment with default fallback value
 FRONTEND_ORIGIN = config('FRONTEND_ORIGIN', cast=CommaSeparatedStrings, default=['http://localhost:3000'])
 GITLAB_TOKEN = config('GITLAB_TOKEN', cast=Secret)
+TRIGGER_TOKEN = config('TRIGGER_TOKEN', cast=Secret)
 GITLAB_URL = config('GITLAB_URL', cast=URL, default='https://gitlab.cern.ch')
 GITLAB_PROJECT="56283"
 PIPELINE_URL = f"https://gitlab.cern.ch/api/v4/projects/{GITLAB_PROJECT}/trigger/pipeline"
