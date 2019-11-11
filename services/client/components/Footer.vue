56a5b49dac48f2d944df7b7c56422bb31267a0d8
Clemens Lange <clemens.lange@cern.ch>
2019-11-11 23:02:56 +0100

    Add simple Footer component that uses GITHUB_SHA env for version display

---
+++ services/client/components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+<footer class="footer">
+  <div class="content has-text-centered">
+    <p>
+      <strong>CMS TDR Diff</strong> by <a href="https://cern.ch/clange">Clemens Lange</a>. Version hash {{ this.$env.GITHUB_SHA }}. Please report issues and feature requests in the <a href="https://github.com/clelange/cms-tdr-diff/issues">GitHub repository</a>.
+    </p>
+  </div>
+</footer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
