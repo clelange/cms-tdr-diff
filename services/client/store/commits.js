035ab1cb507831d055bcda62046cabfc8563a64f
Clemens Lange <clemens.lange@cern.ch>
2019-11-12 22:38:58 +0100

    Comment out console.log statements

--- services/client/store/commits.js
+++ services/client/store/commits.js
@@ -13,7 +13,7 @@ export const state = () => ({
 
 export const mutations = {
   updateCommits: (state, { commits, categoryName, analysisId }) => {
-    console.log('called updateCommits() in commits.js', categoryName, analysisId)
+    // console.log('called updateCommits() in commits.js', categoryName, analysisId)
     state.commitList = commits.commits
     state.projectInfo = commits.project_info
   }
@@ -21,14 +21,14 @@ export const mutations = {
 
 export const actions = {
   async load({ state, commit }, { categoryName, analysisId }) {
-    console.log('called load() in commits.js for category', categoryName, 'and analysis', analysisId)
-    console.log(state.commitList)
+    // console.log('called load() in commits.js for category', categoryName, 'and analysis', analysisId)
+    // console.log(state.commitList)
     // FIXME
     if ((state.commitList) && (categoryName in Object.keys(state.commitList))) {
-      console.log(state.commitList[categoryName].length)
+      // console.log(state.commitList[categoryName].length)
       return
     }
-    console.log('axios commits')
+    // console.log('axios commits')
     commit('setApiStatus', null, { root: true })
     this.$axios.setToken(this.$env.REQUEST_TOKEN)
     await this.$axios.$get('commits/'+categoryName+'/'+analysisId).then(
--- services/client/store/projects.js
+++ services/client/store/projects.js
@@ -12,24 +12,24 @@ export const state = () => ({
 
 export const mutations = {
   updateProjects: (state, { projects, categoryName }) => {
-    console.log('called updateProjects() in projects.js', categoryName)
-    console.log(projects.data.length)
+    // console.log('called updateProjects() in projects.js', categoryName)
+    // console.log(projects.data.length)
     state.myProjects = projects.data
     // console.log(state.projects)
-    console.log(state.myProjects)
+    // console.log(state.myProjects)
     // console.log(state.projects.categoryName[0])
   }
 }
 
 export const actions = {
   async load({ state, commit }, categoryName) {
-    console.log('called load() in projects.js for category', categoryName)
+    // console.log('called load() in projects.js for category', categoryName)
     // FIXME: use list instead, test for content
     if ((state.myProjects) && (categoryName in Object.keys(state.myProjects))) {
-      console.log(state.myProjects[categoryName].length)
+      // console.log(state.myProjects[categoryName].length)
       return
     }
-    console.log('axios projects')
+    // console.log('axios projects')
     commit('setApiStatus', null, { root: true })
     this.$axios.setToken(this.$env.REQUEST_TOKEN)
     await this.$axios.$get('/projects/'+categoryName).then(
