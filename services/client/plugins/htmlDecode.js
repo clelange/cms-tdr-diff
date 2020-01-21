f5821e217b229f82d8052b61a688de7cc6a0c0a9
Clemens Lange <clemens.lange@cern.ch>
2020-01-22 00:01:25 +0100

    Remove client (preparing for submodule)

--- services/client/plugins/htmlDecode.js
+++
@@ -1,8 +0,0 @@
-export default ({ app }, inject) => {
-  // Set the function directly on the context.app object
-  app.htmlDecode = (input) => {
-    var e = document.createElement('div');
-    e.innerHTML = input;
-    return e.childNodes.length === 0 ? "" : e.childNodes[0].nodeValue;
-  }
-}
